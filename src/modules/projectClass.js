653b578d88f05e7404762149e1b70a11546263fe
RohitBhojak <rohitbhojak99@gmail.com>
2025-03-11 17:14:49 +0000

    Create classes for todo and project

---
+++ src/modules/projectClass.js
@@ -0,0 +1,15 @@
+class Project {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+        this.list = [];
+    }
+
+    addTodo(todo) {
+        this.list.push(todo);
+    }
+
+    removeTodo(index) {
+        this.list.splice(index, 1);
+    }
+}
