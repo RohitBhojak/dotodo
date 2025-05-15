fdbd185cce17deb1a21ed9680221662d15541807
RohitBhojak <rohitbhojak99@gmail.com>
2025-05-15 11:50:27 +0000

    Add storage logic to utils

---
+++ src/modules/utils/storage.js
@@ -0,0 +1,43 @@
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      e.name === "QuotaExceededError" &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
+
+const ls = (function () {
+  function store(key, value) {
+    if (storageAvailable("localStorage")) {
+      localStorage.setItem(key, value);
+    } else {
+      console.log("local storage not available");
+    }
+  }
+
+  function retrieve(key) {
+    if (storageAvailable("localStorage")) {
+      return localStorage.getItem(key);
+    } else {
+      console.log("local storage not available");
+    }
+  }
+
+  return {
+    store,
+    retrieve,
+  };
+})();
+
+export default ls;
