d6993c328fa31bf5b698873a003a9916abf3dfd6
RohitBhojak <rohitbhojak99@gmail.com>
2025-05-19 19:12:57 +0000

    Add event listeners for projects and todos

--- src/modules/DOM/events.js
+++ src/modules/DOM/events.js
@@ -1,9 +1,16 @@
 import loadTodo from "./loadTodo";
 import projectUtil from "../utils/projectUtil";
+import todoUtil from "../utils/todoUtil";
+import loadProjectList from "./loadProjectList";
 
 const left = document.querySelector(".left");
 left.addEventListener("click", (e) => {
-  if (e.target.matches("button")) {
+  if (e.target.matches(".deleteProject")) {
+    const project = e.target.closest(".project");
+    const pid = project.dataset.pid;
+    projectUtil.removeProject(pid);
+    loadProjectList();
+  } else if (e.target.matches("button")) {
     left.querySelector(".active").classList.remove("active");
     e.target.classList.add("active");
     loadTodo(e.target.id || e.target.dataset.pid);
@@ -17,5 +24,10 @@ right.addEventListener("click", (e) => {
     const pid = todo.dataset.pid;
     const id = todo.dataset.id;
     projectUtil.removeTodo(pid, id);
+    const active = left.querySelector(".active");
+    loadTodo(active.id || active.pid);
+  } else if (e.target.matches(".toggle")) {
+    const todo = e.target.closest(".todo");
+    todoUtil.toggleTodo(todo);
   }
 });
