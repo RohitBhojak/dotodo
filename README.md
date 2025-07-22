029e041ebeb1883c6b6978aee4545ca5f3353f0e
Rohit Bhojak <rohitbhojak99@gmail.com>
2025-07-22 06:39:44 +0000

    Add eslintConfigPrettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dotodo
+
 Todo-list website
--- webpack.common.js
+++ webpack.common.js
@@ -1,27 +1,27 @@
-const htmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
+  entry: "./src/index.js",
 
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        clean: true,
-    },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true,
+  },
 
-    plugins: [
-        new htmlWebpackPlugin({
-            template: './src/template.html',
-        }),
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
+  },
 };
